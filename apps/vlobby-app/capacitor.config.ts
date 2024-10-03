9c643f0c6312b211b7dd2da1a2c6ba8c36331e97
ZachooBuilds <129256396+ZachooBuilds@users.noreply.github.com>
2024-10-03 22:52:52 +1300

    test without server

--- apps/vlobby-app/capacitor.config.ts
+++ apps/vlobby-app/capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.vlobby.app',
   appName: 'vlobby-app',
   webDir: 'out',
-  server: {
-    url: 'http://192.168.1.64:3000',
-    cleartext: true,
-  },
+  // server: {
+  //   url: 'http://192.168.1.64:3000',
+  //   cleartext: true,
+  // },
 };
 
 export default config;
