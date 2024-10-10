e268c961569501df4604d702c3d4c185caf786fa
ZachooBuilds <129256396+ZachooBuilds@users.noreply.github.com>
2024-10-10 18:19:42 +1300

    fix

--- apps/vlobby-resolve/src/app/_components/file-upload-dropzone-form.tsx
+++ apps/vlobby-resolve/src/app/_components/file-upload-dropzone-form.tsx
@@ -122,7 +122,7 @@ export const FileUploadForm = ({
           </div>
         </div>
       </motion.div>
-      <MultiPhotoCapture onCapture={handleCapturePhotos} />
+      {/* <MultiPhotoCapture onCapture={handleCapturePhotos} /> */}
     </div>
   );
 };
